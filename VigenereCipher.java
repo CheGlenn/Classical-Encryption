9970040e483f8567ecca53acc98e196845aee653
CheGlenn <118393138+CheGlenn@users.noreply.github.com>
2023-10-03 15:38:35 -0500

    Updated VigenereCipher

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -18,6 +18,22 @@ public class VigenereCipher{
       }//else
   }//else
 }//main
+
+//pre-conditions: String word, String key
+//post-conditions: String newKey
+//keyCreation creates a new key by repeating the given key and shortening it to the length of the given word
+public static String keyCreation(String word, String key){
+
+
+  String newKey = key.repeat((word.length() / key.length()) + 1);
+  // . repeat method repeats the word x amount of times
+
+  newKey = newKey.substring(0, word.length());
+  //cuts newKey back down to length of word
+
+  return newKey;
+
+}
   
   //pre-conditions: String s, String k
   //post-conditions: none
@@ -25,84 +41,47 @@ public class VigenereCipher{
   //Encode creates a string from an initial key that is the same size as the plaintext
   //Then uses each corresponding letter from word and new key string to print final ciphertext
   public static void encode(String s, String k){
+
+    if (k.length() < 1){
+      System.err.println("Key of length 0 given");
+    } else{
     
-    //key creation
     int base = 97;
     String word = s; //word for encoding
     String keyWord = k; //keyword for encoding
-    char[] keywordChar = keyWord.toCharArray(); //make char array for key word
-    char[] key; //empty char array for final key
-    key = new char[word.length()]; //set lenght of key to length of word
-    int j = 0; //counter var
+    char[] newWord;
+    newWord = new char[word.length()];
+    for (int i = 0; i < word.length(); i++){
+      char letter = word.charAt(i); //saves char at String(i)
+      char n = keyCreation(word, keyWord).charAt(i); //saves char at String(i)
+       
+      int result = (int) ( letter - base ) + (n - base); //converts letter to corresponding int and calculates new value
+      result = (result % 26); //loop around to start of character range
+      newWord[i] = (char) (result + base);
+      //converts result to corresponding charS and calculates new value
 
-    if(keyWord.length() == 0){ //checks for keyWord, if none then prints word and ends program
-      System.out.println(word);
-      System.exit(1);
-    }
-    else{
-
-      for(int i = 0; i < word.length(); i++){ //loop through word
-        key[i] = keywordChar[j]; //set index i in key to index j in keyword
-        j++; //incrament counter
-
-        if(j>=keyWord.length()){ //check if j is greater than or equal to the length of keyword
-          j = 0; //reset counter to 0
-        }//if
       }//for
-      String finalKey = new String(key);
-      //encode
-      char[] newWord;
-      newWord = new char[word.length()];
-      for (int i = 0; i < word.length(); i++){
-        char letter = word.charAt(i); //saves char at String(i)
-        char n = finalKey.charAt(i); //saves char at String(i)
-        // System.out.println(letter);
-        // System.out.println(n);
-        int result = ((int) letter - base ) + ((int) n - base); //converts letter to corresponding int and calculates new value
-        result = (result % 25); //loop around to start of character range
-        newWord[i] = (char) (result + base);
-        //converts result to corresponding charS and calculates new value
-
-      } //for
-
     System.out.println(new String(newWord)); //prints new word
-  }
-  } //encode
+    }
+  }//encode
 
   //pre-conditions: String s, String k
   //post-conditions: none
   //decode creates a String from an initial key that is the same size as the ciphertext
   //Then uses each corresponding letter from ciphertext and new key string to print final plaintext
   public static void decode(String s, String k){
-    //key creation
     int base = 97;
     String word = s; //word for encoding
     String keyword = k; //keyword for encoding
-    char[] keywordChar = keyword.toCharArray(); //make char array for key word
-    char[] key; //empty char array for final key
-    key = new char[word.length()]; //set lenght of key to length of word
-    int j = 0; //counter var
-
-      for(int i = 0; i < word.length(); i++){ //loop through word
-        key[i] = keywordChar[j]; //set index i in key to index j in keyword
-        j++; //incrament counter
-
-        if(j>=keyword.length()){ //check if j is greater than or equal to the length of keyword
-          j = 0; //reset counter to 0
-        }//if
-      }//for
-      String finalKey = new String(key);
-
-    //decode
     char[] newWord; //initialize array for new encoded word
     newWord = new char[word.length()]; //set length for newWord equal to size of inputted word
     for (int i = 0; i < word.length(); i++){
       char letter = word.charAt(i); //saves char at String(i)
-      char n = finalKey.charAt(i);
-      int result = ((int) letter - base ) - ((int) n - base); //converts letter to corresponding int and calculates new value
-      result = (result % 25); //loop around to start of character range
+      char n = keyCreation(word, keyword).charAt(i);
+      int result = (int) (letter - base ) - (n - base); //converts letter to corresponding int and calculates new value
+      result = (result % 26); //loop around to start of character range
       if(result < 0){ //checks if the resulting num is negative, if so turns the result into number range (25) + resulting number
-        result = 25 + result;
+        result = 26 + result;
       }
       newWord[i] = (char) (result + base);
       //converts result to corresponding chars and calculates new value
